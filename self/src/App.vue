66a9ce835df3be79fecf13b72fd1dee236752f6e
rabbit-tian <1019610885@qq.com>
2018-10-08 16:14:41 +0800

    am

---
+++ self/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+export default {
+  name: 'App',
+  components:{
+    Home,
+  }
+}
+</script>
+
+<style>
+  html,body{
+    height: 100%;
+  }
+  #app{
+    height: 100%;
+  }
+</style>
